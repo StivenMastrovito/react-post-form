efe29c06ad9d057bb159ddfe97037ebc61ba9d7f
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-12-04 14:59:47 +0100

    feat: form

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,19 @@ const initialBlogForm = {
 
 function App() {
   const [formDate, setFormDate] = useState(initialBlogForm);
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([{
+    author: "Stiven",
+    title: "come non sviluppare",
+    body: "Imparate da loris",
+    public: true,
+  },
+{
+    author: "Loris",
+    title: "BOh",
+    body: "BOh Boh BOg BOgBogfosodfsdasfdfsdfsdfdfsd",
+    public: false,
+  }
+]);
 
   function aggiornaForm(event) {
     const key = event.target.name;
@@ -33,27 +45,35 @@ function App() {
     <>
       <h1>BLOG</h1>
       <form action="" onSubmit={addBlog}>
-        <label htmlFor="author">Autore:</label>
-        <input required type="text" name='author' id='author' value={formDate.author} onChange={aggiornaForm} />
-
-        <label htmlFor="title">Titolo:</label>
-        <input required type="text" name='title' id='title' value={formDate.title} onChange={aggiornaForm} />
-
-        <label htmlFor="body">Descrizione:</label>
-        <input required type="text-area" name="body" id="body" value={formDate.body} onChange={aggiornaForm} />
+        <div>
+          <label htmlFor="author">Autore:</label>
+          <input required type="text" name='author' id='author' value={formDate.author} onChange={aggiornaForm} />
+        </div>
+        <div>
+          <label htmlFor="title">Titolo:</label>
+          <input required type="text" name='title' id='title' value={formDate.title} onChange={aggiornaForm} />
+        </div>
+        <div className='align_top flex' >
+          <label htmlFor="body">Descrizione:</label>
+          <textarea className='flex' required type="text-area" name="body" id="body" value={formDate.body} onChange={aggiornaForm} />
+        </div>
 
         <label htmlFor="public">Vuoi pubblicarlo online?</label>
         <input type="checkbox" name="public" id="public" checked={formDate.public} onChange={aggiornaForm} />
-        <button type='submit'>PUBBLICA</button>
+
+        <button className='btn btn_blue' type='submit'>PUBBLICA</button>
       </form>
       <div className="grid">
         {blogs.map((blog) => (
-          <div className="col">
-            <div>
-              <p>{blog.title}</p>
-              <p>{blog.author}</p>
+          <div className={`col ${blog.public ? "border_green" : "border_yellow"}`}>
+            <div className='flex'>
+              <p>{blog.title} - <span className='text_gray'>{blog.author}</span></p>
             </div>
-            <p><strong>Descrizione:</strong>{blog.body}</p>
+            <div>
+              <p><strong>Descrizione:</strong></p>
+              <p>{blog.body}</p>
+            </div>
+
           </div>
         ))}
 
